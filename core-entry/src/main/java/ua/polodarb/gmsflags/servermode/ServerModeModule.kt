dd6625bcea61535da137c83cff5709b8be9ef87c
Kobzar Danyil <65498838+polodarb@users.noreply.github.com>
2026-09-08 11:23:25 +0200

    chore(server-mode): shorten the remote config fetch interval on debug builds

--- core-entry/src/main/java/ua/polodarb/gmsflags/servermode/ServerModeModule.kt
+++ core-entry/src/main/java/ua/polodarb/gmsflags/servermode/ServerModeModule.kt
@@ -3,22 +3,35 @@ package ua.polodarb.gmsflags.servermode
 import com.google.firebase.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.remoteConfig
+import com.google.firebase.remoteconfig.remoteConfigSettings
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import ua.polodarb.gmsflags.BuildConfig
 import ua.polodarb.gmsflags.data.repository.servermode.ServerModeRepository
 import kotlin.coroutines.resume
 
 private const val KEY_OFFLINE_MODE = "android_offline_mode"
+private const val DEBUG_MINIMUM_FETCH_INTERVAL_SECONDS = 0L
 
 val serverModeModule = module {
     single<ServerModeStore> { SharedPreferencesServerModeStore(androidContext()) }
     single<ServerModeRepository> {
+        val remoteConfig = Firebase.remoteConfig.withDebugFetchInterval()
         DefaultServerModeRepository(
             store = get(),
-            fetchRawConfig = { Firebase.remoteConfig.fetchOfflineModePayload() },
+            fetchRawConfig = { remoteConfig.fetchOfflineModePayload() },
         )
     }
+}
+
+private fun FirebaseRemoteConfig.withDebugFetchInterval(): FirebaseRemoteConfig = apply {
+    if (!BuildConfig.DEBUG) return@apply
+    setConfigSettingsAsync(
+        remoteConfigSettings {
+            minimumFetchIntervalInSeconds = DEBUG_MINIMUM_FETCH_INTERVAL_SECONDS
+        },
+    )
 }
 
 private suspend fun FirebaseRemoteConfig.fetchOfflineModePayload(): ServerModeFetch {
